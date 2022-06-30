87d14466f6bfc059d264d27ac8a3680d0a51bae2
KIMYEJU <108450096+KIMYEJU@users.noreply.github.com>
2022-06-30 17:49:54 +0900

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,148 @@
+/*
+2022-06-30
+첫 자바 프로그램
+By YEJU
+*/
+
+
+// import java.util.Scanner;
+
+// class Main {
+//   public static void main(String[] args) {
+
+//     Scanner stdIn = new Scanner(System.in);
+
+//     System.out.print("정숫값:"); //키보드를 통해 입력한 정수값을 받는다
+//     int x = stdIn.nextInt(); //입력받은 정숫값을 x에 저장한다.
+//     System.out.println(x + "를 입력했습니다.");
+
+//   }
+// }
+
+
+import java.util.Scanner; //*대신 Scanner를 써도 됨. Scanner를 사용하기 위해 추가
+
+class Main {
+  public static void main(String[] args) {
+
+   int x, y, z;
+
+    x = 63;
+    y = 18;
+    z = 52;
+
+    System.out.println("x값은 " + x +"입니다.");
+    System.out.println("y값은 " + y +"입니다.");
+    System.out.println("z값은 ")
+
+
+
+
+    
+    // int x;
+    // int y;
+
+    // x = 63;
+    // y = 18;
+
+    // System.out.println("x값은 " + x +"입니다.");
+    // System.out.println("y값은 " + y +"입니다.");
+    // System.out.println("합계는 " + (x + y) + "입니다.");
+    // System.out.println("평균은 " + (x + y) / 2 + "입니다.");
+
+    
+
+    // System.out.println("첫 Java 프로그램입니다." + "화면에 출력하고 있습니다.");
+    
+    // System.out.println("첫 Java 프로그램입니다."); //ln은 줄바꿈이다.
+    // System.out.println("화면에 출력하고 있습니다.");
+    
+    
+    // System.out.println(" 금 ");
+    // System.out.println(" 상 ");
+    // System.out.println(" 첨 ");
+    // System.out.println(" 화 ");
+
+
+
+
+
+    
+//     Scanner stdIn = new Scanner(System.in);
+
+//     System.out.print("정숫값:");
+//     int x = stdIn.nextInt(); //x에 입력받은 정숫값을 저장한다.
+
+//     System.out.println("10을 더한 값은 " + (x + 10)+ "입니다.");
+//     System.out.println("10을 뺀 값은" + (x - 10)+ "입니다.");    
+
+
+
+
+    
+
+    // Scanner stdIn = new Scanner(System.in);
+
+    // System.out.print("정숫값:"); //키보드를 통해 입력한 정수값을 받는다
+    // int x = stdIn.nextInt(); //입력받은 정숫값을 x에 저장한다.
+    // System.out.println(x + "를 입력했습니다.");
+
+
+
+
+
+    
+
+    // Scanner scanner = new Scanner(System.in); //스캐너 객체 생성.
+
+    // System.out.print("57");
+    // String input = scanner.nextLine();
+    // int num = Integer.parseInt(input); //입력받은 문자열을 숫자로 변환
+
+    // System.out.println("입력내용 :"+input);
+    // System.out.printf("num=%d%n", num);
+
+
+
+
+    
+    
+    // System.out.println("Hello world!");  // "Hello World" 출력
+    // //System.out.println("코드의 실행을 중지시킬 경우도 사용");
+
+
+
+    // int x = 10, y = 20; //정수용 변수 x에 10을 할당, y에 20을 할당
+    // int tmp = 0;  //tmp라는 변수에 0을 할당, 초기화 한다.
+
+    // System.out.println("x:"+ x + " y:" + y);  //띄어쓰기도 글자다. x에 x값, y에 y값을 표시하도록 명령한 것.
+
+    // tmp = x; // tmp = 10
+    // x = y;   // x = 20
+    // y = tmp; // y = 10
+
+    // //PY) x, y =y, x  이 한 줄로 끝. 위도 가능.
+
+    // System.out.println("x:"+ x + " y:" + y);
+
+
+    // String name = "Ja" + "va";  //Ja라는 문자열과 va라는 문자열을 붙여서 문자열 변수 name에 할당한다.
+    // String str = name + 8.0; //name이라는 문자열 형 변수값과 8.0이라는 실수를 붙여서 문자열 변수 str에 할당.
+
+    // System.out.println(name);
+    // System.out.println(str);
+    // System.out.println(7 + " ");
+    // System.out.println(" " + 7);
+    // System.out.println(7 + "");
+    // System.out.println("" + 7);
+    // System.out.println("" + "");
+    // System.out.println(7 + 7 + "");
+    // System.out.println("" + 7 + 7);
+
+    // System.out.println("82 + 17 = " + (82 + 17)); //()괄호를 산수처럼 먼저 처리한다.
+    // System.out.println("82 - 17 = " + (82 - 17));
+
+
+    
+  }
+}
